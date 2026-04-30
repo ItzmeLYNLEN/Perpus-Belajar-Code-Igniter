fa649def0724f810eeafbc1ef28b6045e212c93c
unknown <m.daffa342@gmail.com>
2026-04-30 21:31:28 +0700

    create crud admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,8 +13,18 @@ $routes->get('/latihan/if', 'Latihan::contoh_if');
 $routes->get('/latihan/for', 'Latihan::contoh_for');
 $routes->get('/latihan/foreach', 'Latihan::contoh_foreach');
 
-//login admin
-$routes->get('/Admin/login-admin', 'Admin::login');
-$routes->get('/admin/dashboard-admin', 'Admin::dashboard');
+// Routes untuk login dan autentikasi
+$routes->get('/admin/login-admin', 'Admin::login');
 $routes->post('/admin/autentikasi-login', 'Admin::autentikasi');
 $routes->get('/admin/logout', 'Admin::logout');
+
+// Route untuk Dashboard
+$routes->get('/admin/dashboard-admin', 'Admin::dashboard');
+
+// Routes untuk module admin
+$routes->get('/admin/master-data-admin', 'Admin::master_data_admin');
+$routes->get('/admin/input-data-admin', 'Admin::input_data_admin');
+$routes->post('/admin/simpan-admin', 'Admin::simpan_data_admin');
+$routes->get('/admin/edit-data-admin/(:alphanum)', 'Admin::edit_data_admin/$1');
+$routes->post('/admin/update-admin', 'Admin::update_data_admin');
+$routes->get('/admin/hapus-data-admin/(:alphanum)', 'Admin::hapus_data_admin/$1');
--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends BaseController
     {
         return view('Backend/Login/login');
     }
+
     public function autentikasi()
     {
         $modelAdmin = new M_Admin();
@@ -19,21 +20,16 @@ class Admin extends BaseController
 
         if($cekUsername == 0) {
             session()->setFlashdata('error', 'Username Tidak Ditemukan!');
-            ?>
-            <script>history.go(-1);</script>
-            <?php
+            return redirect()->to(base_url('admin/login-admin'));
         } else {
             $dataUser = $modelAdmin->getDataAdmin(['username_admin' => $username, 'is_delete_admin' => '0'])->getRowArray();
             $passwordUser = $dataUser['password_admin'];
 
-            
             $verifikasiPassword = password_verify($password, $passwordUser);
 
             if(!$verifikasiPassword) {
                 session()->setFlashdata('error', 'Password Tidak Sesuai!');
-                ?>
-                <script>history.go(-1);</script>
-                <?php
+                return redirect()->to(base_url('admin/login-admin'));
             } else {
                 $dataSession = [
                     'ses_id'    => $dataUser['id_admin'],
@@ -42,12 +38,11 @@ class Admin extends BaseController
                 ];
                 session()->set($dataSession);
                 session()->setFlashdata('success', 'Login Berhasil!');
-                ?>
-                <script>document.location = "<?= base_url('admin/dashboard-admin'); ?>";</script>
-                <?php
+                return redirect()->to(base_url('admin/dashboard-admin'));
             }
         }
     }
+
     public function dashboard()
     {
        if (session()->get('ses_id') == "" or session()->get('ses_user') == "" or session()->get('ses_level') == "") {
@@ -60,7 +55,7 @@ class Admin extends BaseController
             echo view('Backend/Template/sidebar');
             echo view('Backend/Login/dashboard_admin');
             echo view('Backend/Template/footer');
-    }
+        }
     }
 
     public function logout()
@@ -68,10 +63,184 @@ class Admin extends BaseController
         session()->remove('ses_id');
         session()->remove('ses_user');
         session()->remove('ses_level');
-        session()->setFlashdata('info', 'Logout Berhasil!');
+        session()->setFlashdata('info', 'Anda telah keluar dari sistem!');
         ?>
         <script>document.location = "<?= base_url('admin/login-admin'); ?>";</script>
         <?php
     }
 
+    public function master_data_admin() 
+    {
+        if (session()->get('ses_id') == "" or session()->get('ses_user') == "" or session()->get('ses_level') == "") {
+            session()->setFlashdata('error', 'Silakan login terlebih dahulu!');
+            ?>
+            <script>
+                document.location = "<?= base_url('admin/login-admin'); ?>";
+            </script>
+            <?php
+        } else {
+            $modelAdmin = new M_Admin();
+            $uri = service('uri');
+            $pages = $uri->getSegment(2);
+            
+            $dataUser = $modelAdmin->getDataAdmin(['is_delete_admin' => '0', 'akses_level !=' => '1'])->getResultArray();
+            
+            $data['pages'] = $pages;
+            $data['data_user'] = $dataUser;
+            
+            echo view('Backend/Template/header', $data);
+            echo view('Backend/Template/sidebar', $data);
+            echo view('Backend/MasterAdmin/master-data-admin', $data);
+            echo view('Backend/Template/footer', $data);
+        }
+    }
+
+    public function input_data_admin() 
+    {
+        if (session()->get('ses_id') == "" or session()->get('ses_user') == "" or session()->get('ses_level') == "") {
+            session()->setFlashdata('error', 'Silakan login terlebih dahulu!');
+            ?>
+            <script>
+                document.location = "<?= base_url('admin/login-admin'); ?>";
+            </script>
+            <?php
+        } else {
+            echo view('Backend/Template/header');
+            echo view('Backend/Template/sidebar');
+            echo view('Backend/MasterAdmin/input-admin');
+            echo view('Backend/Template/footer');
+        }
+    }
+
+    public function simpan_data_admin() 
+    {
+        if (session()->get('ses_id') == "" or session()->get('ses_user') == "" or session()->get('ses_level') == "") {
+            session()->setFlashdata('error', 'Silakan login terlebih dahulu!');
+            ?>
+            <script>
+                document.location = "<?= base_url('admin/login-admin'); ?>";
+            </script>
+            <?php
+        } else {
+            $modelAdmin = new M_Admin();
+            $nama = $this->request->getPost('nama');
+            $username = $this->request->getPost('username');
+            $level = $this->request->getPost('level');
+
+            $cekuname = $modelAdmin->getDataAdmin(['username_admin' => $username])->getNumRows();
+            
+            if ($cekuname > 0) {
+                session()->setFlashdata('error', 'Username sudah digunakan!!');
+                ?>
+                <script>
+                    history.go(-1);
+                </script>
+                <?php
+            } else {
+                $hasil = $modelAdmin->autoNumber()->getRowArray();
+                if (!$hasil) {
+                    $id = "ADM001";
+                } else {
+                    $kode = $hasil['id_admin'];
+                    $noUrut = (int) substr($kode, -3);
+                    $noUrut++;
+                    $id = "ADM" . sprintf("%03s", $noUrut);
+                }
+
+                $dataSimpan = [
+                    'id_admin'        => $id,
+                    'nama_admin'      => $nama,
+                    'username_admin'  => $username,
+                    'password_admin'  => password_hash('pass_admin', PASSWORD_DEFAULT),
+                    'akses_level'     => $level,
+                    'is_delete_admin' => '0',
+                    'created_at'      => date('Y-m-d H:i:s'),
+                    'updated_at'      => date('Y-m-d H:i:s')
+                ];
+
+                $modelAdmin->saveDataAdmin($dataSimpan);
+                session()->setFlashdata('success', 'Data Admin Berhasil Ditambahkan !!');
+                ?>
+                <script>
+                    document.location = "<?= base_url('admin/master-data-admin'); ?>";
+                </script>
+                <?php
+            }
+        }
+    }
+
+    public function edit_data_admin() 
+    {
+        $uri = service('uri');
+        $idEdit = $uri->getSegment(3);
+        $modelAdmin = new M_Admin();
+        
+        $dataAdmin = $modelAdmin->getDataAdmin(['sha1(id_admin)' => $idEdit])->getRowArray();
+        session()->set(['idUpdate' => $dataAdmin['id_admin']]);
+        
+        $page = $uri->getSegment(2);
+        $data['page'] = $page;
+        $data['web_title'] = "Edit Data Admin";
+        $data['data_admin'] = $dataAdmin;
+        
+        echo view('Backend/Template/header', $data);
+        echo view('Backend/Template/sidebar', $data);
+        echo view('Backend/MasterAdmin/edit-admin', $data);
+        echo view('Backend/Template/footer', $data);
+    }
+
+    public function update_data_admin() 
+    {
+        $modelAdmin = new M_Admin();
+        $idUpdate = session()->get('idUpdate');
+        $nama = $this->request->getPost('nama');
+        $level = $this->request->getPost('level');
+
+        if ($nama == "" or $level == "") {
+            session()->setFlashdata('error', 'Isian tidak boleh kosong!!');
+            ?>
+            <script>
+                history.go(-1);
+            </script>
+            <?php
+        } else {
+            $dataUpdate = [
+                'nama_admin'  => $nama,
+                'akses_level' => $level,
+                'updated_at'  => date("Y-m-d H:i:s")
+            ];
+            
+            $whereUpdate = ['id_admin' => $idUpdate];
+            $modelAdmin->updateDataAdmin($dataUpdate, $whereUpdate);
+            
+            session()->remove('idUpdate');
+            session()->setFlashdata('success', 'Data Admin Berhasil Diperbaharui!');
+            ?>
+            <script>
+                document.location = "<?= base_url('admin/master-data-admin'); ?>";
+            </script>
+            <?php
+        }
+    }
+
+    public function hapus_data_admin() 
+    {
+        $modelAdmin = new M_Admin();
+        $uri = service('uri');
+        $idHapus = $uri->getSegment(3);
+
+        $dataUpdate = [
+            'is_delete_admin' => '1',
+            'updated_at'      => date("Y-m-d H:i:s")
+        ];
+        $whereUpdate = ['sha1(id_admin)' => $idHapus];
+        $modelAdmin->updateDataAdmin($dataUpdate, $whereUpdate);
+
+        session()->setFlashdata('success', 'Data Admin Berhasil Dihapus!');
+        ?>
+        <script>
+            document.location = "<?= base_url('admin/master-data-admin'); ?>";
+        </script>
+        <?php
+    }
 }
